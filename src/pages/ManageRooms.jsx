59c203a7ce8ad26acd8e7cb01972a1b06f2e43e4
Akash Maheema <Akashmaheema@gmail.com>
2024-11-14 00:11:35 +0530

    update all table styles

--- src/pages/ManageRooms.jsx
+++ src/pages/ManageRooms.jsx
@@ -29,19 +29,19 @@ const ManageRooms = () => {
   return (
     <div className="bg-white shadow-md rounded-lg p-6">
       <h2 className="text-3xl font-bold text-[#003366] mb-6">Manage Rooms</h2>
-      <table className="min-w-full border border-gray-300">
+      <table className="min-w-full  border-gray-300">
         <thead>
           <tr>
-            <th className="border px-4 py-2 text-center font-semibold text-white bg-[#003366]">
+            <th className="px-4 py-2 text-center font-semibold text-white bg-[#003366]">
               Room Name
             </th>
-            <th className="border px-4 py-2 text-center font-semibold text-white bg-[#003366]">
+            <th className="px-4 py-2 text-center font-semibold text-white bg-[#003366]">
               Capacity
             </th>
-            <th className="border px-4 py-2 text-center font-semibold text-white bg-[#003366]">
+            <th className="px-4 py-2 text-center font-semibold text-white bg-[#003366]">
               Occupants
             </th>
-            <th className="border px-4 py-2 text-center font-semibold text-white bg-[#003366]">
+            <th className="px-4 py-2 text-center font-semibold text-white bg-[#003366]">
               Actions
             </th>
           </tr>
@@ -55,11 +55,14 @@ const ManageRooms = () => {
             </tr>
           ) : (
             rooms.map((room) => (
-              <tr key={room.id} className="border-b border-gray-200">
-                <td className="border-r px-4 py-2">{room.room}</td>
-                <td className="border-r px-4 py-2">{room.capacity}</td>
-                <td className="border-r px-4 py-2">{room.occupants}</td>
-                <td className="border-r px-4 py-2">
+              <tr
+                key={room.id}
+                className="border-b bg-[#E6EBF0] border-[#E1E1E1]"
+              >
+                <td className="text-center px-4 py-2">{room.room}</td>
+                <td className="text-center px-4 py-2">{room.capacity}</td>
+                <td className="text-center px-4 py-2">{room.occupants}</td>
+                <td className="text-center px-4 py-2">
                   <button
                     onClick={() => handleDeleteRoom(room.id)}
                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
