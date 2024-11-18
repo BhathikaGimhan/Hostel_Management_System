752a003771bc95af7dc77ec07abd28fd78ebe149
Akash Maheema <Akashmaheema@gmail.com>
2024-11-18 20:52:55 +0530

    update z index

--- src/pages/UserDashBoard.jsx
+++ src/pages/UserDashBoard.jsx
@@ -80,12 +80,10 @@ function StudentDashboard() {
 
   return (
     <div className="bg-white shadow-md rounded-lg p-6">
-      <h1 className="text-4xl font-bold text-center text-blue-700 mb-10">
-        Student Dashboard
-      </h1>
+      <h2 className="text-2xl font-bold mb-6">Dashboard</h2>
 
       {/* Main Grid Layout */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10 ">
         {/* Student Details */}
 
         {studentDetails && (
